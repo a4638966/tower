e522a4e62e08505fa74fef20ea98abbe16e8c1d0
a4638966 <2971714942@qq.com>
2019-05-27 10:11:25 +0800

    修改客户信息数据内容

--- scripts/demand-detail.js
+++ scripts/demand-detail.js
@@ -1,14 +1,40 @@
-var DemandDetail = function() {
+var DemandDetail = function () {
     this._urls = {};
-    this._control = {};
+    this._control = {
+        company1: $('company1'),
+        company2: $('company2'),
+    };
 }
 
-DemandDetail.prototype.initControl = function() {
+DemandDetail.prototype.initControl = function () {
     var that = this;
-
-    // 初始化layui
-    layui.use(['form', 'element'], function() {
+    var companyNames = ['河南旭东点气科技有限公司', '新乡市公安局交通管理支队'];
+    layui.use(['form', 'element'], function () {
         var form = layui.form;
         var element = layui.element;
     });
+    $('#company1').on('mouseenter', function () {
+        layer.tips(companyNames[0], '#company1', {
+            tips: [1, '#061a3f']
+        });
+
+    });
+    $('#company1').on('mouseout', function () {
+        layer.close(layer.tips(companyNames[0], '#company1', {
+            tips: [1, '#061a3f']
+        }))
+    });
+
+    $('#company2').on('mouseenter', function () {
+        layer.tips(companyNames[1], '#company2', {
+            tips: [1, '#061a3f']
+        });
+
+    });
+    $('#company2').on('mouseout', function () {
+        layer.close(layer.tips(companyNames[1], '#company2', {
+            tips: [1, '#061a3f']
+        }))
+    });
+
 }
