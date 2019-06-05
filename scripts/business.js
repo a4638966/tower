fc69782c8db64ebd891aedf9422bf40c950e32ec
a4638966 <2971714942@qq.com>
2019-06-06 00:06:50 +0800

    添加地图返回事件

--- scripts/business.js
+++ scripts/business.js
@@ -108,6 +108,50 @@ Business.prototype.initChart = function () {
         document.addEventListener('DOMMouseScroll', scrollFunc, false);
     } //W3C
     window.onmousewheel = document.onmousewheel = scrollFunc; //IE/Opera/Chrome
+    // 定义一个控件类,即function
+    function ZoomControl() {
+        // 默认停靠位置和偏移量
+        this.defaultAnchor = BMAP_ANCHOR_TOP_LEFT;
+        this.defaultOffset = new BMap.Size(10, 10);
+    }
+
+    // 通过JavaScript的prototype属性继承于BMap.Control
+    ZoomControl.prototype = new BMap.Control();
+
+    // 自定义控件必须实现自己的initialize方法,并且将控件的DOM元素返回
+    // 在本方法中创建个div元素作为控件的容器,并将其添加到地图容器中
+    ZoomControl.prototype.initialize = function (map) {
+        // 创建一个DOM元素
+        var div = document.createElement("div");
+        // 添加文字说明
+        div.appendChild(document.createTextNode("返回"));
+        // 设置样式
+        div.style.cursor = "pointer";
+        div.style.border = "1px solid #1E9FFF";
+        div.style.backgroundColor = "#1E9FFF";
+        div.style.color = "#fff";
+        div.style.padding = "5px 10px";
+        div.style.borderRadius = '10px';
+        // 绑定事件,点击一次放大两级
+        div.onclick = function (e) {
+            getBoundary('河南省');
+            getMap(provinceId, cityId, prefectureId);
+            that._commonData.provinceId = 17;
+            that._commonData.cityId = '';
+            that._commonData.prefectureId = '';
+            that.energyStation();
+            that.prolongStation();
+        }
+        // 添加DOM元素到地图中
+        map.getContainer().appendChild(div);
+        // 将DOM元素返回
+        return div;
+    }
+    // 创建控件
+    var myZoomCtrl = new ZoomControl();
+    // 添加到地图当中
+    map.addControl(myZoomCtrl);
+
     // 声明一个数组，装行政区域的数据
     var blist = [];
     // 设置一个计数器，用来判断什么时候加载完成行政区域，然后画图
