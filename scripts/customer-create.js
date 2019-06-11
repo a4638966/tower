28fa5d9dd728e9d16e2dd58eebe6a22529e39cf3
a4638966 <2971714942@qq.com>
2019-06-12 00:07:49 +0800

    添加用户新建功能

--- scripts/customer-create.js
+++ scripts/customer-create.js
@@ -1,34 +1,162 @@
 var CustomerCreate = function () {
     this._urls = {};
-    this._controls = {};
+    this._controls = {
+        userTrueName: $('#userTrueName'),
+        contractCode: $('#contractCode'),
+        userIphone: $('#userIphone'),
+        processDate: $('#processDate'),
+        devType: $('#devType'),
+        signer: $('#signer'),
+        money: $('#money'),
+        beginTime: $('#beginTime'),
+        ddlProvince: $('#ddlProvince'),
+        ddlCidy: $('#ddlCidy'),
+        ddlprefecture: $('#ddlprefecture'),
+        readyTime: $('#readyTime'),
+        address: $('#address'),
+        processCycle: $('#processCycle'),
+        btnSave: $('#btnSave')
+    };
+    this._commonData = {
+        form: null,
+        provinceId: '',
+        cityId: '',
+        prefectureId: '',
+        devType: ''
+    }
 };
 CustomerCreate.prototype.initControl = function () {
     var that = this;
     layui.use(['form', 'laydate', 'element', 'upload'], function () {
         var form = layui.form;
+        that._commonData.form = layui.form;
         var laydate = layui.laydate;
         var element = layui.element;
         var upload = layui.upload;
         laydate.render({
-            elem: '#test1' //指定元素
+            elem: '#processDate' //指定元素
         });
         laydate.render({
-            elem: '#test2' //指定元素
+            elem: '#beginTime' //指定元素
         });
-        var uploadInst = upload.render({
-            elem: '#uploadData',
-            url: '/upload/',
-            accept: 'file',
-            done: function (res) {
-                //上传完毕回调
-            },
-            error: function () {
-                //请求异常回调
-                layer.msg('没有上传接口，上传不成功')
+        form.on('select(ddlProvince)', function (data) {
+            if (data.value != '') {
+                that._commonData.provinceId = data.value;
+                that.getCity();
+            } else {
+                that._controls.ddlCidy.html('<option value="">请选择</option>');
+                that._controls.ddlprefecture.html('<option value="">请选择</option>');
+                that._commonData.form.render()
+                that._commonData.cityId = '';
+                that._commonData.prefectureId = '';
             }
+        });
+        form.on('select(ddlCidy)', function (data) {
+            if (data.value != '') {
+                that._commonData.cityId = data.value;
+                that.getPrefecture();
+            } else {
+                that._controls.ddlprefecture.html('<option value="">请选择</option>');
+                that._commonData.form.render()
+                that._commonData.cityId = '';
+                that._commonData.prefectureId = '';
+            }
+        });
+        form.on('select(ddlprefecture)', function (data) {
+            that._commonData.prefectureId = data.value;
+        });
+
+        form.on('select(devType)', function (data) {
+            that._commonData.devType = data.value;
         });
     });
     var formSelects = layui.formSelects;
     formSelects.render('selectId');
     formSelects.render('selectId1');
+
+    that._controls.btnSave.on('click', function () {
+        that.saveData();
+    });
+}
+
+CustomerCreate.prototype.getCity = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/area/shi',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            provinceId: that._commonData.provinceId
+        },
+        success: function (res) {
+            if (res.code === 200) {
+                var str = '<option value="">请选择</option>';
+                for (var i = 0; i < res.result.length; i++) {
+                    str += '<option value="' + res.result[i].id + '">' + res.result[i].name + '</option>'
+                }
+                that._controls.ddlCidy.html(str);
+                that._commonData.form.render()
+            }
+        }
+    });
+
+}
+
+CustomerCreate.prototype.getPrefecture = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/area/qx',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            cityId: that._commonData.cityId
+        },
+        success: function (res) {
+            if (res.code === 200) {
+                var str = '<option value="">请选择</option>';
+                for (var i = 0; i < res.result.length; i++) {
+                    str += '<option value="' + res.result[i].id + '">' + res.result[i].name + '</option>'
+                }
+                that._controls.ddlprefecture.html(str);
+                that._commonData.form.render()
+            }
+        }
+    });
+};
+
+CustomerCreate.prototype.saveData = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/customer_center/add_customer',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            userTrueName: that._controls.userTrueName.val(),
+            contractCode: that._controls.contractCode.val(),
+            userIphone: that._controls.userIphone.val(),
+            processDate: that._controls.processDate.val(),
+            devType: that._commonData.devType,
+            money: that._controls.money.val(),
+            beginTime: that._controls.beginTime.val(),
+            provinceId: that._commonData.provinceId,
+            cityId: that._commonData.cityId,
+            prefectureId: that._commonData.prefectureId,
+            signer: that._controls.signer.val(),
+            readyTime: that._controls.readyTime.val(),
+            address: that._controls.address.val(),
+            processCycle: that._controls.processCycle.val(),
+        },
+        success: function (res) {
+            console.log(res)
+        }
+    });
 }
