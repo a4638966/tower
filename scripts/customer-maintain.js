6cdcb8dcd2ca7ae2b6a9de6698e673a7401f3bad
a4638966 <2971714942@qq.com>
2019-06-12 11:17:15 +0800

    '添加客户查询代码'

--- scripts/customer-maintain.js
+++ scripts/customer-maintain.js
@@ -2,8 +2,28 @@ var CustomerMaintain = function () {
     this._urls = {};
     this._controls = {
         searchItem: $('.search-box > dd'),
-        startDate: $('#startDate'),
-        endDate: $('#endDate')
+
+        ddlProvince: $('#ddlProvince'),
+        ddlCidy: $('#ddlCidy'),
+        ddlprefecture: $('#ddlprefecture'),
+
+        beginTime: $('#beginTime'),
+        endTime: $('#endTime'),
+
+        userTrueName: $('#userTrueName'),
+        address: $('#address'),
+        customerContact: $('#customerContact'),
+        customerServer: $('#customerServer'),
+
+        btnSearch: $('#btnSearch')
+    };
+    this._commonData = {
+        form: null,
+        provinceId: '',
+        cityId: '',
+        prefectureId: '',
+        devType: '25',
+        customerType: ''
     }
 };
 CustomerMaintain.prototype.initControl = function () {
@@ -11,7 +31,38 @@ CustomerMaintain.prototype.initControl = function () {
     layui.use(['laydate','element', 'form'], function () {
         var laydate = layui.laydate;
         var form = layui.form;
+        that._commonData.form = layui.form;
         var element = layui.element;
+        form.on('select(ddlProvince)', function (data) {
+            if (data.value != '') {
+                that._commonData.provinceId = data.value;
+                that.getCity();
+            } else {
+                that._controls.ddlCidy.html('<option value="">客户所在城市</option>');
+                that._controls.ddlprefecture.html('<option value="">客户所在区县</option>');
+                that._commonData.form.render()
+                that._commonData.cityId = '';
+                that._commonData.prefectureId = '';
+            }
+        });
+        form.on('select(ddlCidy)', function (data) {
+            if (data.value != '') {
+                that._commonData.cityId = data.value;
+                that.getPrefecture();
+            } else {
+                that._controls.ddlprefecture.html('<option value="">客户所在区县</option>');
+                that._commonData.form.render()
+                that._commonData.cityId = '';
+                that._commonData.prefectureId = '';
+            }
+        });
+        form.on('select(ddlprefecture)', function (data) {
+            that._commonData.prefectureId = data.value;
+        });
+
+        form.on('select(customerType)', function (data) {
+            that._commonData.customerType = data.value;
+        });
         laydate.render({
             elem: '#startDate'
         });
@@ -24,6 +75,92 @@ CustomerMaintain.prototype.initControl = function () {
         if ($(this).attr('class') != 'date-box') {
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
+            that._commonData.devType = $(this).attr('devType');
+        }
+    });
+
+    this.handleSearch();
+
+    this._controls.btnSearch.on('click', function () {
+        that.handleSearch();
+    })
+}
+
+CustomerMaintain.prototype.getCity = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/area/shi',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            provinceId: that._commonData.provinceId
+        },
+        success: function (res) {
+            if (res.code === 200) {
+                var str = '<option value="">客户所在城市</option>';
+                for (var i = 0; i < res.result.length; i++) {
+                    str += '<option value="' + res.result[i].id + '">' + res.result[i].name + '</option>'
+                }
+                that._controls.ddlCidy.html(str);
+                that._commonData.form.render()
+            }
+        }
+    });
+
+}
+
+CustomerMaintain.prototype.getPrefecture = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/area/qx',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            cityId: that._commonData.cityId
+        },
+        success: function (res) {
+            if (res.code === 200) {
+                var str = '<option value="">客户所在区县</option>';
+                for (var i = 0; i < res.result.length; i++) {
+                    str += '<option value="' + res.result[i].id + '">' + res.result[i].name + '</option>'
+                }
+                that._controls.ddlprefecture.html(str);
+                that._commonData.form.render()
+            }
+        }
+    });
+};
+
+CustomerMaintain.prototype.handleSearch = function () {
+    var that = this;
+    $.ajax({
+        url: 'http://www.baoxingtech.com:9604/sys/customer_center/select_customer',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Admin-Token': $.cookie('adminToken')
+        },
+        data: {
+            provinceId: that._commonData.provinceId,
+            cityId: that._commonData.cityId,
+            prefectureId: that._commonData.prefectureId,
+            devType: that._commonData.devType,
+            beginTime: that._controls.beginTime.val(),
+            endTime: that._controls.endTime.val(),
+            userTrueName: that._controls.userTrueName.val(),
+            customerType: that._commonData.customerType,
+            address: that._controls.address.val(),
+            customerContact: that._controls.customerContact.val(),
+            customerServer: that._controls.customerServer.val()
+        },
+        success: function (res) {
+            console.log(res)
         }
     });
 }
