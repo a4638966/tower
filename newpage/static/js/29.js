a956f54be4c498046c3b500c78fe0f9defba06e5
wangx <wangxiaowei753@126.com>
2019-06-21 17:15:35 +0800

    newpage

---
+++ newpage/static/js/29.js
@@ -0,0 +1,2 @@
+webpackJsonp([29],{F5Ap:function(e,t){},TLzg:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"business"},[t("router-view")],1)},staticRenderFns:[]};var s=n("VU/8")({data:function(){return{}},components:{},created:function(){},mounted:function(){},methods:{}},r,!1,function(e){n("F5Ap")},"data-v-084e7004",null);t.default=s.exports}});
+//# sourceMappingURL=29.js.map?5a1aed536810284c552b
